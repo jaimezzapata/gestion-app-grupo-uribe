ac0fa3310048f48246075bfde8bccb23f9216134
jaimezzapata <zapataval2304@gmail.com>
2024-04-10 16:28:26 -0500

    Se crea peticion get de json-server

--- front/src/components/layouts/pages/usuarios/ListarUsuarios.jsx
+++ front/src/components/layouts/pages/usuarios/ListarUsuarios.jsx
@@ -1,17 +1,26 @@
 import axios from "axios";
+import { useState } from "react";
 let apiUsuarios = "http://localhost:5174/usuarios";
 
 const ListarUsuarios = () => {
+  const [usuarios, setUsuarios] = useState([]);
   const getUsuarios = async () => {
     let resultado = await axios.get(apiUsuarios);
-    console.log(resultado.data);
+    setUsuarios(resultado.data);
   };
-
+  console.log(usuarios);
   return (
     <div>
       <button onClick={getUsuarios} type="button">
         Listar Usuario
       </button>
+      {usuarios.map((usuario) => {
+        <section>
+          <p>Usuario: {usuario.usuario}</p>
+          <p>Contraseña: {usuario.contrasena}</p>
+          <p>Correo: {usuario.correo}</p>
+        </section>;
+      })}
     </div>
   );
 };
